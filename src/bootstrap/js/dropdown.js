a6870ce3bd10ce2b17ac587be780e57c21cdd47e
kelui <xzero92v2@gmail.com>
2014-11-19 11:49:52 -0500

    reuploading files

--- src/bootstrap/js/dropdown.js
+++
@@ -1,8 +0,0 @@
-$(".dropdown-menu li a").click(function(){
-    var selText = $(this).text();
-    $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
-});
-
-$("#btnSearch").click(function(){
-    alert($('.btn-select').text()+", "+$('.btn-select2').text());
-});
--- src/js/Notifications.js
+++
@@ -1,5 +0,0 @@
-function myFunction()
-{
-    document.getElementById("herdzz").innerHTML = " ";
-
-}
