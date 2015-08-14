255a4b09cf45a584d2f143876264fdef9698e576
Tessa Shum <tshum741@gmail.com>
2015-08-14 13:56:50 -0400

    admin account addition

--- application/controllers/testing.php
+++ application/controllers/testing.php
@@ -29,4 +29,14 @@ class testing extends CI_Controller{
                 $this->load->view('createwrev',$result);     
 	}
 	
+	public function ticketstub(){
+		$this->load->library('path');
+            	$path = $this->path->getPath();
+            	$this->load->library('session');
+		$nav_data = $this->session->all_userdata();
+                $result = array_merge($path,$nav_data);
+                $this->load->view('Create_Wrevel_View',$result);
+                $this->load->view('ticketstub',$result);     
+	}
+	
 }	
