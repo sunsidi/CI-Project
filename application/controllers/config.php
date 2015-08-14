68832e5105097c2835f86d3acc1ad284ebba84b5
Tessa Shum <tshum741@gmail.com>
2015-08-14 13:56:50 -0400

    admin account addition

--- application/controllers/config.php
+++ application/controllers/config.php
@@ -2,8 +2,10 @@
 require_once('Stripe.php');
 
 $stripe = array(
-  "secret_key"      => "sk_live_AJHUFwVnx9aiOOutcEz27HnE",
-  "publishable_key" => "pk_live_5ggqGwcspXMWKOape7yW0DTq"
+  //"secret_key"      => "sk_live_AJHUFwVnx9aiOOutcEz27HnE",
+  //"publishable_key" => "pk_live_5ggqGwcspXMWKOape7yW0DTq"
+  "secret_key"      => "sk_test_5nhXB2o5OOktfe6ywQdZflSW",
+"publishable_key" => "pk_test_vb73aZ9B2HmQtnyBoPaCNyTK"
 );
 
 
